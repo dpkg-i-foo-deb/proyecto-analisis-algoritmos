5a41168d1719fed30f1a2b6020ed659ccd5f93f7
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-13 10:56:04 -0500

    Fix models

--- api/models/exam.go
+++ api/models/exam.go
@@ -8,17 +8,17 @@ import (
 
 type Exam struct {
 	gorm.Model
-	Points      int           `json:"points"`
-	Name        string        `json:"name"`
-	Description string        `json:"description"`
-	LimitTime   time.Duration `json:"limitTime"`
-	Questions   []Question    `json:"question" gorm:"many2many:exam_questions"`
-	Students    []Student     `json:"students" gorm:"many2many:exam_presentations"`
+	Points      int        `json:"points"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	LimitTime   time.Time  `json:"limitTime"`
+	Questions   []Question `json:"question" gorm:"many2many:exam_questions"`
+	Students    []Student  `json:"students" gorm:"many2many:exam_presentations"`
 }
 
 type ExamPresentations struct {
-	ExamID    uint          `json:"exam" gorm:"primaryKey"`
-	StudentID uint          `json:"student" gorm:"primaryKey"`
-	Grade     float32       `json:"grade"`
-	Time      time.Duration `json:"time"`
+	ExamID    uint      `json:"exam" gorm:"primaryKey"`
+	StudentID uint      `json:"student" gorm:"primaryKey"`
+	Grade     float32   `json:"grade"`
+	Time      time.Time `json:"time"`
 }
