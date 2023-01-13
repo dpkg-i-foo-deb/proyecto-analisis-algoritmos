bf40dd41dfee2fbfda2b0b1f2c38971a9da09266
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-13 10:55:10 -0500

    Models...

--- api/database/migrate_models.go
+++ api/database/migrate_models.go
@@ -6,12 +6,19 @@ import (
 )
 
 func migrateModels() {
-	err := DB.AutoMigrate(
+
+	err := DB.SetupJoinTable(&models.Exam{}, "Students", &models.ExamPresentations{})
+
+	util.HandleErrorStop(err)
+
+	err = DB.AutoMigrate(
 		&models.User{},
 		&models.Administrator{},
 		&models.Student{},
 		&models.Question{},
 		&models.Option{},
+		&models.Exam{},
+		&models.ExamPresentations{},
 	)
 
 	util.HandleErrorStop(err)
--- api/models/exam.go
+++ api/models/exam.go
@@ -13,4 +13,12 @@ type Exam struct {
 	Description string        `json:"description"`
 	LimitTime   time.Duration `json:"limitTime"`
 	Questions   []Question    `json:"question" gorm:"many2many:exam_questions"`
+	Students    []Student     `json:"students" gorm:"many2many:exam_presentations"`
+}
+
+type ExamPresentations struct {
+	ExamID    uint          `json:"exam" gorm:"primaryKey"`
+	StudentID uint          `json:"student" gorm:"primaryKey"`
+	Grade     float32       `json:"grade"`
+	Time      time.Duration `json:"time"`
 }
