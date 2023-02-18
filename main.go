fb255b0d66542835323ca6eea1c10959b2109ee0
Linux User <developer@localhost.my.domain>
2023-02-18 10:04:42 -0500

    Generador matrices

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Matriz struct {
+	Datos [][]int `json:"datos"`
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	matriz := generateMatrix(1000, 1000)
+
+	file, _ := os.Create("Matriz_" + strconv.Itoa(1000) + "x" + strconv.Itoa(1000) + ".json")
+	writeMatrix(file, matriz)
+	defer file.Close()
+
+	file2, _ := os.Open("Matriz_" + strconv.Itoa(1000) + "x" + strconv.Itoa(1000) + ".json")
+	readMatrix(file2)
+	defer file2.Close()
+}
+
+func generateMatrix(rows int, cols int) Matriz {
+	datos := make([][]int, rows)
+
+	for i := 0; i < rows; i++ {
+		datos[i] = make([]int, cols)
+		
+		for j := 0; j < cols; j++ {
+			datos[i][j] = rand.Intn(10000)
+		}
+	}
+
+	matriz := Matriz{Datos: datos}
+
+	return matriz
+}
+
+func writeMatrix(file *os.File, m Matriz) {
+	encoder := json.NewEncoder(file)
+
+	encoder.Encode(m)
+}
+
+func readMatrix(file *os.File) Matriz {
+	decoder := json.NewDecoder(file)
+
+	matriz := Matriz{}
+
+	decoder.Decode(&matriz)
+
+	return matriz
+}
