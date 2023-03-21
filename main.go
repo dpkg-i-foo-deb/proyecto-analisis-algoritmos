c5ba4cd37fab0239b996483354b5cd44311f285e
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-21 15:17:39 -0500

    Eliminar  la basura del main

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"generador/utilidades"
-	"math/rand"
-	"os"
-	"strconv"
-	"time"
-)
-
 func main() {
 
-	generar()
-}
-
-func generar() {
-	rand.Seed(time.Now().UnixNano())
-
-	matriz := utilidades.GenerateMatrix(1000, 1000)
-
-	file, _ := os.Create("Matriz_" + strconv.Itoa(1000) + "x" + strconv.Itoa(1000) + ".json")
-	utilidades.WriteMatrix(file, matriz)
-	defer file.Close()
-
-	file2, _ := os.Open("Matriz_" + strconv.Itoa(1000) + "x" + strconv.Itoa(1000) + ".json")
-	utilidades.ReadMatrix(file2)
-	defer file2.Close()
 }
