6c69aab0f7a28e8105548e40576f448db1f3f6dd
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-25 11:37:15 -0500

    Generar matrices nxn donde n=2^m donde m va de 1 a 12

--- utilidades/generacion.go
+++ utilidades/generacion.go
@@ -1,13 +1,29 @@
 package utilidades
 
 import (
+	"math"
 	"math/rand"
+	"os"
+	"strconv"
 
 	"time"
 )
+
+const CASOS_PRUEBA = 12
 
 func GenerarMatrices() {
 
 	rand.Seed(time.Now().UnixNano())
 
+	for i := 1; i <= CASOS_PRUEBA; i++ {
+		cantidad := int64(math.Pow(2.0, float64(i)))
+
+		matriz := generateMatrix(int(cantidad), int(cantidad))
+
+		archivo, err := os.Create("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + ".json")
+
+		VerificarError(err)
+
+		writeMatrix(archivo, matriz)
+	}
 }
