eff528365815ae3c2febdad20a07a2647d06659c
Laura Suárez <lauraa.suarezg@uqvirtual.edu.co>
2023-03-25 02:09:05 -0500

    Implementación del método WinogradScaled

---
+++ algoritmos/multiplicar_escalar_test.go
@@ -0,0 +1,29 @@
+package algoritmos_test
+
+import (
+	"generador/algoritmos"
+	"reflect"
+	"testing"
+)
+
+func TestMultiplicarEscalar(t *testing.T) {
+
+	A := [][]int{
+		{1, 2},
+		{3, 4},
+	}
+
+	Resultado := make([][]int, len(A))
+
+	for i := range Resultado {
+		Resultado[i] = make([]int, len(A[0]))
+	}
+	alpha := 2
+
+	algoritmos.MultiplicarEscalar(A, Resultado, alpha)
+
+	expected := [][]int{{2, 4}, {6, 8}}
+	if !reflect.DeepEqual(Resultado, expected) {
+		t.Errorf("Expected %v but got %v", expected, Resultado)
+	}
+}
