d82908fbb2f399cd43d2ada62c1064c6f32f2958
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-25 14:24:40 -0500

    Agregar IV_4_Pararell_Block

---
+++ test/iv_4_parallel_block_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"generador/algoritmos"
+	"reflect"
+	"testing"
+)
+
+func TestIV_4_ParallelBlock(t *testing.T) {
+	a := [][]int{
+		{1, 2},
+		{4, 5},
+	}
+
+	b := [][]int{
+		{10, 11},
+		{13, 14},
+	}
+
+	esperado := [][]int{
+		{36, 39},
+		{105, 114},
+	}
+
+	resultado := algoritmos.IV_4_Pararell_Block(a, b)
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("IV_4 ParallelBlock ha fallado")
+	}
+}
