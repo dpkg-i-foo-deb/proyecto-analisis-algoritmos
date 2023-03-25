677673b07075914a916b2bc720b7ace54a3cf028
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-25 12:09:53 -0500

    Leer todas las matrices

--- test/generacion_test.go
+++ test/generacion_test.go
@@ -2,15 +2,34 @@ package test
 
 import (
 	"generador/utilidades"
+	"math"
+	"os"
+	"strconv"
 	"testing"
 )
 
+const CASOS_PRUEBA = 12
+
 func TestGeneracionLecturaMatrices(t *testing.T) {
+
+	defer eliminarMatrices()
+
 	utilidades.GenerarMatrices()
 
 	matrices := utilidades.LeerMatrices()
 
-	if cap(matrices) < 12 {
-		t.Error("Las matrices no son suficientes, fueron generadas ", cap(matrices))
+	if len(matrices) != 12 {
+		t.Error("Las matrices no son suficientes, fueron generadas ", len(matrices))
+	}
+}
+
+func eliminarMatrices() {
+	for i := 1; i <= CASOS_PRUEBA; i++ {
+		cantidad := int64(math.Pow(2.0, float64(i)))
+
+		err := os.Remove("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + ".json")
+
+		utilidades.VerificarError(err)
+
 	}
 }
--- utilidades/lectura.go
+++ utilidades/lectura.go
@@ -12,7 +12,7 @@ func LeerMatrices() []modelos.Matriz {
 	var archivos []os.File
 	var matrices []modelos.Matriz
 
-	for i := 1; i < CASOS_PRUEBA; i++ {
+	for i := 1; i <= CASOS_PRUEBA; i++ {
 		cantidad := int64(math.Pow(2.0, float64(i)))
 
 		archivo, err := os.Open(("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + ".json"))
