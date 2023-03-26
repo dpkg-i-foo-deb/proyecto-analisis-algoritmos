a425b0cbcb4eef370c711bd7165668038defb088
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-26 11:08:56 -0500

    Leer matrices a y b

--- utilidades/lectura.go
+++ utilidades/lectura.go
@@ -7,24 +7,40 @@ import (
 	"strconv"
 )
 
-func LeerMatrices() []modelos.Matriz {
+func LeerMatrices() ([]modelos.Matriz, []modelos.Matriz) {
 
-	var archivos []os.File
-	var matrices []modelos.Matriz
+	var archivosA []os.File
+	var archivosB []os.File
+	var matricesA []modelos.Matriz
+	var matricesB []modelos.Matriz
 
 	for i := 1; i <= CASOS_PRUEBA; i++ {
 		cantidad := int64(math.Pow(2.0, float64(i)))
 
-		archivo, err := os.Open(("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + ".json"))
+		archivo, err := os.Open(("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + "_a" + ".json"))
 
 		VerificarError(err)
 
-		archivos = append(archivos, *archivo)
+		archivosA = append(archivosA, *archivo)
 	}
 
-	for i := range archivos {
-		matrices = append(matrices, readMatrix(&archivos[i]))
+	for i := 1; i <= CASOS_PRUEBA; i++ {
+		cantidad := int64(math.Pow(2.0, float64(i)))
+
+		archivo, err := os.Open(("matriz_" + strconv.FormatInt(cantidad, 10) + "x" + strconv.FormatInt(cantidad, 10) + "_b" + ".json"))
+
+		VerificarError(err)
+
+		archivosB = append(archivosB, *archivo)
 	}
 
-	return matrices
+	for i := range archivosA {
+		matricesA = append(matricesA, readMatrix(&archivosA[i]))
+	}
+
+	for i := range archivosB {
+		matricesB = append(matricesB, readMatrix(&archivosB[i]))
+	}
+
+	return matricesA, matricesB
 }
