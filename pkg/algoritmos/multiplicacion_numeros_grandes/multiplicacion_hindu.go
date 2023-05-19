b29f613b018493f3ceed848bce2e04df6278de14
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2023-05-19 01:37:12 -0500

    implemetacion de mult hindu recursiva y su test

--- pkg/algoritmos/multiplicacion_numeros_grandes/multiplicacion_hindu.go
+++ pkg/algoritmos/multiplicacion_numeros_grandes/multiplicacion_hindu.go
@@ -45,3 +45,41 @@ func MultiplicacionHinduIterativa(n1, n2 []int, resultado []int) []int {
 
 	return resultado
 }
+
+/*
+ * Multiplicación Hindu Recursiva usando estructuras estáticas.
+ *  Parámetros:
+ *  n1: Arreglo de enteros con los dígitos del multiplicando.
+ *  n2: Arreglo de enteros con los dígitos del multiplicador.
+ *  resultado: Arreglo de enteros con los dígitos para el resultado.
+ * 		Sus dimensiones deben ser la suma de la longitud de los arreglos de multiplicando y multiplicador.
+ */
+
+func MultiplicacionHinduRecursiva(n1, n2 []int, resultado []int, i, j int) []int {
+	if i < len(n1) {
+		if j < len(n2) {
+			temp := n1[len(n1)-1-i] * n2[len(n2)-1-j]
+			resultado[len(resultado)-1-i-j] += temp
+			return MultiplicacionHinduRecursiva(n1, n2, resultado, i, j+1)
+		}
+		return MultiplicacionHinduRecursiva(n1, n2, resultado, i+1, 0)
+	}
+
+	return LlevarAcarreos(resultado, len(resultado)-1)
+}
+
+func LlevarAcarreos(resultado []int, i int) []int {
+	if i > 0 {
+		if resultado[i] >= 10 {
+			resultado[i-1] += resultado[i] / 10
+			resultado[i] %= 10
+		}
+		return LlevarAcarreos(resultado, i-1)
+	}
+
+	if resultado[0] == 0 {
+		resultado = resultado[1:]
+	}
+
+	return resultado
+}
