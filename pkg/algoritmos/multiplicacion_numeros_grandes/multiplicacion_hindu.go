933c13a3269a6dbc097a81bd352a24fb992ee2c6
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2023-05-19 01:19:37 -0500

    implemetacion de mult hindu iterativa y su test

--- pkg/algoritmos/multiplicacion_numeros_grandes/multiplicacion_hindu.go
+++ pkg/algoritmos/multiplicacion_numeros_grandes/multiplicacion_hindu.go
@@ -1,23 +1,47 @@
 package multiplicacion_numeros_grandes
 
-func MultiplicacionHindu(n1 []int, n2 []int) []int {
-	a := arrayToInt(n1)
-	b := arrayToInt(n2)
-	resultado := 0
+/*
+ * Integrantes:
+ *  Stiven Herrera Sierra.
+ *  Mateo Estrada
+ *  Laura Suárez.
+ *  Daniel Eduardo Puerta.
+ */
 
+/*
+ * Multiplicación Hindu iterativa usando estructuras estáticas.
+ *  Parámetros:
+ *  n1: Arreglo de enteros con los dígitos del multiplicando.
+ *  n2: Arreglo de enteros con los dígitos del multiplicador.
+ *  resultado: Arreglo de enteros con los dígitos para el resultado.
+ * 		Sus dimensiones deben ser la suma de la longitud de los arreglos de multiplicando y multiplicador.
+ */
+
+func MultiplicacionHinduIterativa(n1, n2 []int, resultado []int) []int {
+
+	//corremos los dígitos de ambos números de derecha a izquierda
 	for i := 0; i < len(n1); i++ {
 		for j := 0; j < len(n2); j++ {
-			resultado += n1[i] * n2[j] * pow(10, len(n1)-i-1+len(n2)-j-1)
+			// Multiplicamos cada dígito de n1 con cada dígito de n2
+			temp := n1[len(n1)-1-i] * n2[len(n2)-1-j]
+			// Almacenamos el resultado en una posición del slice resultado
+			resultado[len(resultado)-1-i-j] += temp
 		}
 	}
 
-	return intToSlice(resultado)
-}
-
-func pow(base int, exponent int) int {
-	result := 1
-	for i := 0; i < exponent; i++ {
-		result *= base
+	// Llevamos los acarreos hacia la izquierda
+	for i := len(resultado) - 1; i > 0; i-- {
+		if resultado[i] >= 10 {
+			// Llevamos el acarreo al dígito anterior y dejamos el resto en el dígito actual
+			resultado[i-1] += resultado[i] / 10
+			resultado[i] %= 10
+		}
 	}
-	return result
+
+	// Eliminamos cualquier cero que pueda haber al principio del slice resultado
+	if resultado[0] == 0 {
+		resultado = resultado[1:]
+	}
+
+	return resultado
 }
