0d7186122197cc248ffa3385eaf61fb552c8b197
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-05-21 20:21:08 -0500

    constantes para multiplicación de números grandes

--- pkg/modelos/resultado_multiplicacion_grandes.go
+++ pkg/modelos/resultado_multiplicacion_grandes.go
@@ -1,3 +1,20 @@
 package modelos
 
 type AlgoritmoMultiplicacionNumerosGrandes string
+
+const (
+	AMERICANA_ITERATIVA_ESTATICO AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Americana Iterativa Estática"
+	AMERICANA_ITERATIVA_DINAMICO AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Americana Iterativa Dinámica"
+	AMERICANA_RECURSIVA_ESTATICO AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Americana Recursiva Estática"
+	AMERICANA_RECURSIVA_DINAMICO AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Americana Recursiva Dinámica"
+	INGLESA_ITERATIVA_ESTATICO   AlgoritmoMultiplicacionNumerosGrandes = "Multilpicación Inglesa Iterativa Estática"
+	INGLESA_ITERATIVA_DINAMICO   AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Inglesa Iterativa Dinámica"
+	INGLESA_RECURSIVA_ESTATICO   AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Inglesa Recursiva Estática"
+	INGLESA_RECURSIVA_DINAMICO   AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Inglesa Recursiva Dinámica"
+	RUSA                         AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Rusa Estática"
+	HINDU                        AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Hindú Estática"
+	EGIPCIA                      AlgoritmoMultiplicacionNumerosGrandes = "Multipicación Egipcia Estática"
+	KARATSUBA                    AlgoritmoMultiplicacionNumerosGrandes = "Karatsuba Recursivo"
+	CADENAS                      AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Representada con Cadenas"
+	DIVIDE_Y_VENCERAS            AlgoritmoMultiplicacionNumerosGrandes = "Multiplicación Utilizando Descomposiciones"
+)
